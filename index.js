0df0de540de10ccaa14ba36e34e5bc91b7012b87
AtoLrn <antoine.lorin2@gmail.com>
2022-09-27 14:08:19 +0200

    git(init): first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import TicTacToe from './src/modules/tictactoe.js'
+
+
+const applications = [
+    TicTacToe
+]
+
+console.log('Application', applications)
---
+++ src/modules/tictactoe.js
@@ -0,0 +1,21 @@
+
+export default class TicTacToe {
+    name = "TicTacToe"
+    url = "https://urldelicone.com"
+
+    constructor() {
+
+    }
+
+    build = () => {
+        console.log('build')
+    }
+
+    export = () => {
+        
+    }
+
+    hide = () => {
+        console.log('hide')
+    }
+}
