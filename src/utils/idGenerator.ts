22178f66f2ac877281016a3801559fe5be8a3267
AtoLrn <antoine.lorin2@gmail.com>
2023-02-28 14:32:07 +0100

    fix: app id + tictactoe size

--- src/utils/idGenerator.ts
+++ src/utils/idGenerator.ts
@@ -1,10 +1,10 @@
 export const generateId = () => {
-    return `${shortId()}-${shortId()}-${shortId()}`
+    return `app-${shortId()}-${shortId()}-${shortId()}`
 }
 
 export const shortId = (length = 9) => {
     return (Math.random() + 1).toString(36).substring(length)
 }
 export const shortNumberId = () => {
-    return Math.round(Math.random() * (100000000 - 0))
+    return Math.round(Math.random() * (100000000))
 }
